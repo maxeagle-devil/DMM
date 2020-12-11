f11b7c8125ef068a6d44866284b77f996a897251
maxeagle-devil <75847463+maxeagle-devil@users.noreply.github.com>
2020-12-11 20:13:30 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd3823700ea36001e3b91b7",
+				"5fd3823700ea36001e3b91b9"
+			]
+		},
+		"5fd3823700ea36001e3b91b7": {
+			"id": "5fd3823700ea36001e3b91b7",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd3823700ea36001e3b91b9": {
+			"id": "5fd3823700ea36001e3b91b9",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/404.js
@@ -0,0 +1,72 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section background="#000000">
+			<Text
+				margin="0px 0 24px 0"
+				text-align="center"
+				font="--headline2"
+				md-font="--headline3"
+				background="#000000"
+				color="#f4a318"
+			>
+				DMM
+			</Text>
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--base"
+				font-weight="700"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override
+					slot="link"
+					text-decoration="none"
+					color="--lightD2"
+					padding="6px 12px"
+					text-transform="uppercase"
+					background="#0c0c0c"
+				/>
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px" />
+				<Override slot="link-index">
+					ERROR NOT FOUND
+				</Override>
+			</Menu>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
